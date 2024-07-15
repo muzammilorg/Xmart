b16b78597c5ea2c5fc723abdedc4cf57c8b5484a
Muzammil <muzammilrazaofficial@gmail.com>
2024-07-15 13:38:24 +0500

    first commit

---
+++ src/routes/routes.jsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from "react-router-dom";
+import { HomePage } from "../pages/home/home";
+
+
+export const publicRoutes = createBrowserRouter([
+    {path: '/', element: <HomePage></HomePage>}
+])
